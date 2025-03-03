0a872eaa64ecd0d6e2050182fd3763388c35e151
Habanero <habanerospices@gmail.com>
2025-03-03 10:42:42 +0100

    Add support for sshrc.d/, and improve error message

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
 	"encoding/base64"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
+	"path/filepath"
 )
 
 const maxSize = 65536 // 64KB limit
@@ -15,6 +20,52 @@ func encodeFileToBase64(path string) (string, error) {
 		return "", err
 	}
 	return base64.StdEncoding.EncodeToString(content), nil
+}
+
+func compressDirectory(directory string) (*bytes.Buffer, error) {
+	buffer := new(bytes.Buffer)
+	gzipWriter := gzip.NewWriter(buffer)
+	tarWriter := tar.NewWriter(gzipWriter)
+
+	err := filepath.Walk(directory, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		relPath, err := filepath.Rel(directory, path)
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+		file, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		header, err := tar.FileInfoHeader(info, "")
+		if err != nil {
+			return err
+		}
+		header.Name = relPath
+		if err := tarWriter.WriteHeader(header); err != nil {
+			return err
+		}
+		if _, err := io.Copy(tarWriter, file); err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	if err := tarWriter.Close(); err != nil {
+		return nil, err
+	}
+	if err := gzipWriter.Close(); err != nil {
+		return nil, err
+	}
+	return buffer, nil
 }
 
 func main() {
@@ -34,14 +85,28 @@ func main() {
 		os.Exit(1)
 	}
 
+	var sshrcDEncoded string
+
+	sshrcDPath := sshHome + "/.sshrc.d"
+	if _, err := os.Stat(sshrcDPath); !os.IsNotExist(err) {
+		sshrcDCompressedBuffer, err := compressDirectory(sshrcDPath)
+		if err != nil {
+			panic(err)
+		}
+		sshrcDEncoded = base64.StdEncoding.EncodeToString(sshrcDCompressedBuffer.Bytes())
+	} else {
+		sshrcDEncoded = ""
+	}
+
 	sshrcEncoded, err := encodeFileToBase64(sshrcPath)
 	if err != nil {
 		fmt.Println("Failed to encode .sshrc:", err)
 		os.Exit(1)
 	}
 
-	if len(sshrcEncoded) > maxSize {
-		fmt.Println("Maximum size(encoded) reached for .sshrc: >", maxSize)
+	if len(sshrcEncoded)+len(sshrcDEncoded) > maxSize {
+		fmt.Printf("Error: Combined size of .sshrc (%.2f KB) and .sshrc.d/ (%.2f KB) exceeds the maximum allowed size of %.f KB.\n",
+			float64(len(sshrcEncoded))/1024, float64(len(sshrcDEncoded))/1024, float64(maxSize)/1024)
 		os.Exit(1)
 	}
 
@@ -68,10 +133,11 @@ func main() {
 	trap "rm -rf $SSHRCCLEANUP; exit" 0
 	echo '%s' | base64 --decode > $SSHHOME/sshrc
 	echo '%s' | base64 --decode > $SSHHOME/sshrc.bashrc
+	echo '%s' | base64 --decode | tar mxzf - -C $SSHHOME 2>/dev/null
 	exec bash --rcfile $SSHHOME/sshrc.bashrc
 	`
 
-	sshCommand := fmt.Sprintf(tmpScript, sshrcEncoded, bashrcEncoded)
+	sshCommand := fmt.Sprintf(tmpScript, sshrcEncoded, bashrcEncoded, sshrcDEncoded)
 
 	sshArgs := os.Args[1:]
 	cmdArgs := append(sshArgs, "-t", "--", sshCommand)
